d0948406d240f5eb238672f1f9ad945be511e08d
Собаева Екатерина <kat9228@yandex.ru>
2022-07-17 13:47:03 +0300

    HW done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,51 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...mark) {
+  for (let i=0; i< mark.length; i++) {
+    if (this.marks === undefined) {
+     this.marks = [mark[0]];
+    } else {
+      this.marks.push(mark[i]);
+   }
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  
+  for (let i=0; i< this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  
+  const avg = sum/this.marks.length;
+  return avg;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;
+}
+
+
+let student2 = new Student("Buzz", "female", 35);
+    student2.setSubject("Geometry");
+    student2.addMarks(2,3,2);
+    console.log(student2);
