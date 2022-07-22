a30ed21df9a6e3ff656266547b69a9302711cd3d
Собаева Екатерина <kat9228@yandex.ru>
2022-07-22 19:30:48 +0300

    Task 1 and 2 done

---
+++ 5.classes/task.js
@@ -0,0 +1,107 @@
+class PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this.state = 100;
+		this.type = null;
+	}
+
+	fix() {
+		this.state *= 1.5;
+	}
+
+	set state(state) {
+		if (state < 0) {
+			this._state = 0;
+		} else if (state > 100) {
+			this._state = 100;
+		} else {
+			this._state = state;
+		}
+	}
+
+	get state() {
+		return this._state;
+	}
+}
+
+class Magazine extends PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.state = 100;
+		this.type = "magazine";
+	}
+
+
+
+}
+
+class Book extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.state = 100;
+		this.type = "book";
+		this.author = author;
+	}
+}
+
+class NovelBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.state = 100;
+		this.type = "novel";
+	}
+}
+
+class FantasticBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.state = 100;
+		this.type = "fantastic";
+	}
+}
+
+class DetectiveBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.state = 100;
+		this.type = "detective";
+	}
+}
+
+
+
+
+class Library {
+	constructor(name) {
+		this.name = name;
+		this.books = [];
+	}
+
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		}
+	}
+
+	findBookBy(type, value) {
+		if (this.books.find(item => item[type] === value) !== undefined) {
+			return this.books.find(item => item[type] === value);
+		} else {
+		 	return null;
+		 }
+				
+	}
+
+	giveBookByName(bookName) {
+		if (this.books.every(item => item.name !== bookName)) {
+			return null;
+		} else {
+			const book = this.books.find(findBook => findBook.name === bookName);
+			this.books = this.books.filter(findBook => findBook.name !== bookName);
+			return book;
+		}
+	}
+}
+
