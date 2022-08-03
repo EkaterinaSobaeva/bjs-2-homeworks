1c33284960143acf4c381ad55315ddfc8b699afa
Собаева Екатерина <kat9228@yandex.ru>
2022-08-03 22:14:58 +0300

    Task done

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,59 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  const cache = {};
+
+  return function(...args) {
+    let keys = Object.keys(cache);
+    if (keys.length > 5) {
+        let key = keys[0];
+        delete cache[key];
+      }
+
+    const hash = args.join(',');
+    let result = cache[hash];
+    
+    if (result === undefined) {
+      cache[hash] = result = func.call(this,...args);
+      return ('Вычисляем: ' + result);
+    } else {
+      return ('Из кэша: ' + result);
+    }
+
+    return result;
+  };
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timerId = false;
+ 
+  return function(...args) {
+    if (timerId) return;
+
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      func.apply(this,args);
+      timerId = false;
+      console.timeEnd('time');
+    }, ms);
+  }
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+
+function debounceDecorator2(func, ms) {
+  let timerId = false;
+  let count;
+
+
+  return function(...args) {
+    count += 1; 
+    if (timerId) return;
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      func.apply(this,args);
+      timerId = false;
+      console.timeEnd('time');
+    }, ms);
+    return count;
+  }
 }
